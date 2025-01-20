0f8065318c07cdd872b2e14f2c15c3906b58f2eb
Jeroen Dries <driesj@driesj.vitovm.rscluster.vito.be>
2025-01-20 09:31:39 +0100

    modis fixes

--- src/modis10A/mod10A_download.py
+++ src/modis10A/mod10A_download.py
@@ -1,24 +1,27 @@
+from pathlib import Path
+
 import nest_asyncio
 from pystac_client import Client
 import pystac
 import planetary_computer
 import stac_asset
+import stac_asset.blocking
 
 collection_url = "https://planetarycomputer.microsoft.com/api/stac/v1/collections/modis-10A1-061"
 
 import json
 from shapely.geometry import shape
-with open("senales.geojson") as f:
+
+with open(Path(__file__).parent.parent.parent / "resources" / "senales.geojson") as f:
     data = json.load(f)
 
 
-#%%
 aoi = shape(data["features"][0]["geometry"])
 
 client = Client.open("https://planetarycomputer.microsoft.com/api/stac/v1",modifier=planetary_computer.sign_inplace)
 item_search = client.search(
    collections=["modis-10A1-061"],
-   max_items=4,
+   max_items=40,
     datetime="2017/2018",
       intersects=aoi
 )
--- src/modis10A/upload_items.py
+++ src/modis10A/upload_items.py
@@ -12,11 +12,15 @@ class BearerAuth(requests.auth.AuthBase):
         return r
 
 #oidc-gen --pub terrascope --flow=device --client-id=public --iss=https://sso.terrascope.be/auth/realms/terrascope
-os.environ["OIDC_SOCK"]="/tmp/oidc-agent-service-1000/oidc-agent.sock"
+#os.environ["OIDC_SOCK"]="/tmp/oidc-agent-service-1000/oidc-agent.sock"
 token, issuer, expires_at = agent.get_token_response("terrascope")
 
 auth = BearerAuth(token)
 
 items = pystac.ItemCollection.from_file("/data/open/modis-10A1-061/item-collection.json")
 
-response = requests.post("https://stac.openeo.vito.be/collections/modis-10A1-061/bulk_items", auth=auth,json={"items":items.to_dict()})
+item_map = { i["id"]:i for i in items.to_dict()["features"]}
+
+response = requests.post("https://stac.openeo.vito.be/collections/modis-10A1-061/bulk_items", auth=auth,json={"items":item_map})
+print(response)
+print(response.json())
